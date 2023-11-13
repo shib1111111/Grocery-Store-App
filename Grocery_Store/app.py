0efdd9b3aa653836ed38eae71cfba1096ddc7ce5
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2023-11-13 17:19:11 +0530

    Update app.py

--- Grocery_Store/app.py
+++ Grocery_Store/app.py
@@ -3,4 +3,5 @@ from application import create_app
 if __name__=="__main__":
     app = create_app()
     
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(debug=False,host = '0.0.0.0')
